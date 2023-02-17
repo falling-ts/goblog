7095fa5f2f5a6ef9f85d940f497a3218f5c8cf8d
yuanshang <zgh.yuanshang@gmail.com>
2023-02-18 03:27:24 +0800

    feat: 移除 URL 路径中的斜杠

--- main.go
+++ main.go
@@ -3,37 +3,55 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+	if err != nil {
+		return
+	}
 }
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+func aboutHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	if err != nil {
+		return
+	}
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-
+func notFoundHandler(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+	_, err := fmt.Fprint(w, "<h1>请求页面未找到 :(</h1><p>如有疑惑，请联系我们。</p>")
+	if err != nil {
+		return
+	}
 }
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	fmt.Fprint(w, "文章 ID："+id)
+	_, err := fmt.Fprint(w, "文章 ID："+id)
+	if err != nil {
+		return
+	}
 }
 
-func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "访问文章列表")
+func articlesIndexHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprint(w, "访问文章列表")
+	if err != nil {
+		return
+	}
 }
 
-func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+func articlesStoreHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprint(w, "创建新的文章")
+	if err != nil {
+		return
+	}
 }
 
 func forceHTMLMiddleware(next http.Handler) http.Handler {
@@ -41,6 +59,18 @@ func forceHTMLMiddleware(next http.Handler) http.Handler {
 		// 1. 设置标头
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		// 2. 继续处理请求
+		next.ServeHTTP(w, r)
+	})
+}
+
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1. 除首页以外，移除所有请求路径后面的斜杆
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+
+		// 2. 将请求传递下去
 		next.ServeHTTP(w, r)
 	})
 }
@@ -67,5 +97,8 @@ func main() {
 	articleURL, _ := router.Get("articles.show").URL("id", "1")
 	fmt.Println("articleURL: ", articleURL)
 
-	http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe(":3000", removeTrailingSlash(router))
+	if err != nil {
+		return
+	}
 }
